a1867cc49ae3123f95dd8a7382e3928eb90e1392
eun bi <dmsql54871@gmail.com>
2021-01-12 04:08:24 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//slide2
+$(function(){
+   $('.intro_dining .slide2').slick({
+     arrows: false, //화살표
+     dots: false, //인디케이스효과
+     fade: false,//페이드효과,
+     autoPlay:true,//자동재생
+     autoplaySpeed:4000,//재생시간
+     pauseOnHover:false,//마우스 호버시 정지
+     pauseOnFocus:false//포커스시정지
+   });
+});
